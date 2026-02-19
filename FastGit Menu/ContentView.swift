bf32336242c8a11a029457f3f9909db8208331aa
Erencan Arica <erencan_arica@hotmail.com>
2026-02-19 17:25:54 +0100

    Initial Commit

---
+++ FastGit Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastGit Menu
+//
+//  Created by Erencan Arica on 19.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastGit Menu/FastGit_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastGit_MenuApp.swift
+//  FastGit Menu
+//
+//  Created by Erencan Arica on 19.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FastGit_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
